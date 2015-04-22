5fd0af3def89d6bf3656fc8650abe7a15c6b69d8
adamheslop <adam_m_heslop@hotmail.com>
2015-04-22 17:57:25 +0100

    Changed draw function name to 'paw'

--- src/testApp.h
+++ src/testApp.h
@@ -28,7 +28,7 @@ class testApp : public ofBaseApp{
 	public:
 		void setup();
 		void update();
-		void draw();
+		void paw();
         void drawColumns();
         void drawColumn( int id );
 		void exit();
